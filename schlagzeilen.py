407a4ea82107208e728c108ab413afeba6cd6e5e
Jan Böhringer <janboe@gmail.com>
2009-10-09 17:41:05 +0200

    updates

--- schlagzeilen.py
+++ schlagzeilen.py
@@ -9,12 +9,15 @@ class bild(object):
 	def __init__(self):
 		self.inter = ["Endlich", "Skandal", "Panik", "Unglaublich", "Horror", "Schrecklich"]
 		self.pre = ["Ex", "Ober", "Hitler", "Ausländer", "Jugend", "Kirchen", "Bayern",
-				"Fußball", "Islamisten", "Amerika", "Meister", "Politiker", "Horror", "Panik", "Todes"]
+				"Fußball", "Islamisten", "Amerika", "Meister", "Politiker", "Horror", "Panik", "Todes",
+				"Hass", "UFO", "Monster", "Katholiken"]
 		self.nomen = ["Frau", "Papst", "Bibel", "Kanzler", "Obama", "Party", "Poldi", "Hitler",
 				"Sex", "Moslem", "Bauer", "Erfinder", "Minister", "Superstar", "Deutschland", 
-				"Verona", "Bohlen", "Merkel", "Wurst", "Priester", "Ausland", "Europa", "Döner", "Klinsi"]
+				"Verona", "Bohlen", "Merkel", "Wurst", "Priester", "Ausland", "Europa", "Döner", "Klinsi",
+				"Killer", "Verbrecher"]
 		self.verben = ["schlägt", "rettet", "kauft", "klaut", "isst", "verlässt", "will", "hasst",
-				"predigt", "befreit", "erschießt", "erfindet", "heiratet", "verbrennt", "kaut"]
+				"predigt", "befreit", "erschießt", "erfindet", "heiratet", "verbrennt", "kauft",
+				"verbietet", "verkauft", "findet", "zerstört"]
 		self.nomen_ende = ["Bayern", "Döner", "Bahn", "Fußball", "RTL", "Dschungel"]
 		self.nomen_ende.extend(self.nomen)
 	def gib(self):
@@ -25,6 +28,9 @@ class bild(object):
 		t.append(self.verben[rand(0,len(self.verben) - 1)] + " ")
 		t.append(self.nomen_ende[rand(0,len(self.nomen_ende) - 1)])
 		return t[0] + t[1] + t[2] + t[3] + t[4]
+		
+	def max(self):
+		return len(self.pre) * len(self.inter) * len(self.nomen) * len(self.verben) * len(self.nomen_ende)
 		
 class Simple(resource.Resource):
     isLeaf = True
@@ -38,27 +44,31 @@ class Simple(resource.Resource):
 <head>
 <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">
 <meta http-equiv="Content-Style-Type" content="text/css">
-<title>BILD informiert</title>
+<title>BIDL informiert</title>
 </head>
 <body>
 <center>
+<h1> Aktuell: </h1>
 		"""
-		self.footer = """
-		
-		
-		
+		self.footer = """		
+		<br>
+		<br> 
 		<a href="javascript:this.location.reload();">refresh</a> - 
 		<a href="http://github.com/janboe/schlagzeilen/blob/master/schlagzeilen.py">source</a>
 		<p><a href="http://www.validome.org/referer">
 			<img style="border:none"
 				 src="http://www.validome.org/images/set5/valid_html_4_0_1.gif"
 				 alt="Valid HTML 4.01" width="80" height="15"></a></p>
+		<br>
+		  <span style="font-size:60%"><i> Mögliche Schlagzeilen: 				 
+		""" + str(self.bild.max()) + """
+		</i></span><br>
 		</center>
 </body>
 </html>
 		"""
     def render_GET(self, request):
-        return self.header + "<h1>" + self.bild.gib() + "</h1>" + self.footer
+        return self.header + self.bild.gib()  + self.footer
 
 if __name__ == '__main__':
 	site = server.Site(Simple())
